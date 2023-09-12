6a09c7637940854306d3441d476ebb55739470a9
Kwasi O-T <tweneboah21@gmail.com>
2023-09-12 17:14:52 +0000

    created state & setstates to handle form fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,23 +5,47 @@ export class Form extends Component {
         super(props)
         
         this.state = {
-            
+            name: "",
+            email: "",
+            password: ""
         }
     }
     
+    handleNameChange = (e) => {
+        this.setState ({
+            name: e.target.value
+        })
+    }
+
+    handleEmailChange = (e) => {
+        this.setState ({
+            email: e.target.value
+        })
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 Login Form
                 <div className='inputs'>
                     <label>Name</label>
-                    <input type="text" name='name'/>
+                    <input type="text" name='name' placeholder='John Smith' value={this.state.name} onChange={this.handleNameChange}/>
                     <label>Email</label>
-                    <input type="email" name='email'/>
+                    <input type="email" name='email' placeholder='example@example.com' value={this.state.email} onChange={this.handleEmailChange}/>
                     <label>Password</label>
-                    <input type="password" name='password'/>
+                    <input type="password" name='password' value={this.state.password} onChange={this.handlePasswordChange}/>
                 </div>
-                <div> 
+                <div className='button'> 
                     <button type="submit">Submit</button>
                 </div>
             </form>
