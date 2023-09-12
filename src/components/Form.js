22dd09025ba8ec382fcc114793f681fa92597555
Kwasi O-T <tweneboah21@gmail.com>
2023-09-12 11:53:01 +0000

    created form component

---
+++ src/components/Form.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            
+        }
+    }
+    
+    render() {
+        return (
+            <form>
+                <div>
+            
+                </div>
+            </form>
+        )
+    }
+}
+
+export default Form
