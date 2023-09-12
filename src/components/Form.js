59c89abc483eab551362aa7ed9586b0dd805ff49
Kwasi O-T <tweneboah21@gmail.com>
2023-09-12 13:29:09 +0000

    created html form inside Formjs

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,8 +12,17 @@ export class Form extends Component {
     render() {
         return (
             <form>
-                <div>
-            
+                Login Form
+                <div className='inputs'>
+                    <label>Name</label>
+                    <input type="text" name='name'/>
+                    <label>Email</label>
+                    <input type="email" name='email'/>
+                    <label>Password</label>
+                    <input type="password" name='password'/>
+                </div>
+                <div> 
+                    <button type="submit">Submit</button>
                 </div>
             </form>
         )
